196654731f2f67f35ee721fcbe795c846c5adcc3
yes-2875 <mirzafarrukh97@gmail.com>
2026-03-30 20:29:06 -0400

    Fix config for pages export

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,8 @@ const nextConfig = {
   /* config options here */
   reactCompiler: true,
   reactStrictMode: true,
+  output: "export",
+  basePath: process.env.PAGES_BASE_PATH
 };
 
 export default nextConfig;
